03b6a2ec07e343680a1c8d57617f625df6ca4e4c
hexbreak <theownerproject@gmail.com>
2021-01-08 16:57:56 +0000

    Updates

---
+++ src/js/component/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Footer() {
+	return (
+		<div
+			className="container-fluid bg-dark text-light d-flex justify-content-center"
+			style={{ padding: "2em" }}>
+			<span>Copyright @ Your Website 2021</span>
+		</div>
+	);
+}
--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,16 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import { Home } from "./component/home.js";
+import { Navbar } from "./component/navbar.js";
+import { Content } from "./component/content.js";
+import { Footer } from "./component/footer.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(
+	<div>
+		<Navbar />
+		<Content />
+		<Footer />
+	</div>,
+	document.querySelector("#app")
+);
